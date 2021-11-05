3ea4951ff428a58737144b5f46ef282af761a8e0
José Carlos Martins <zemartins81@gmail.com>
2021-11-04 21:22:43 -0300

    chore: update nextAuth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,32 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna'
+
+import { query as q} from 'faunadb'
+
+export default NextAuth({
+    providers: [
+        Providers.GitHub({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            scope: 'read:user',
+        }),
+    ],
+    callbacks: {
+        async signIn(user, account, profile) {
+            const {email} = user
+            try {
+               await fauna.query(
+                    q.Create(
+                        q.Collection('users'),
+                        {data: {email}}
+                    )
+                )
+                return true
+            } catch {
+                return false
+            }
+        }
+    }
+})
