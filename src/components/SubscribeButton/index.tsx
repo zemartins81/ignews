c8e42cdc4502909f4eb97efad02a9e5cfeff685e
José Carlos Martins <zemartins81@gmail.com>
2021-11-04 22:13:55 -0300

    chore: add subscribe.ts

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,26 @@
 import styles from './styles.module.scss'
+import {func} from "prop-types";
+import {useSession, signIn} from "next-auth/client";
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
+    const [session] = useSession()
+    function handleSubscribe() {
+        if(!session) {
+            signIn('github').then(r => {
+
+            })
+            return
+        }
+    }
     return (
         <button
         type="button"
         className={styles.subscribeButton}
+        onClick={handleSubscribe}
         >
             Subscribe now
         </button>
